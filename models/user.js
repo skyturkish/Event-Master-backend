8d2823c0e14ea6de3b302332ee8060041fe46b76
skyturkish <gokturk.acr2002@gmail.com>
2024-06-04 02:36:26 +0300

    Update UserSchema to include additional Discord user fields and methods

--- models/user.js
+++ models/user.js
@@ -1,12 +1,47 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  name: {
+  id: {
     type: String,
     required: true,
+  },
+  bot: {
+    type: Boolean,
+  },
+  system: {
+    type: Boolean,
+  },
+  flags: {
+    type: Number,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  globalName: {
+    type: String,
+  },
+  discriminator: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  banner: {
+    type: String,
+  },
+  accentColor: {
+    type: String,
+  },
+  avatarDecoration: {
+    type: String,
   },
 })
 
 UserSchema.plugin(require('mongoose-autopopulate'))
+
+UserSchema.methods.getFullTag = function () {
+  return `${this.username}#${this.discriminator}`
+}
 
 module.exports = mongoose.model('User', UserSchema)
