b3f225e1c72ef00c7a4d1c9e42f44d698578c25c
skyturkish <gokturk.acr2002@gmail.com>
2024-06-08 13:15:15 +0300

    Add participant update method

--- services/event-service.js
+++ services/event-service.js
@@ -6,6 +6,23 @@ class EventService extends BaseService {
   async findByGuildId(guild) {
     return this.model.find({ guild })
   }
+
+  async addOrUpdateParticipant(eventId, participantId, status = 'invited') {
+    const event = await this.find(eventId)
+    if (!event) {
+      throw new Error('Event not found')
+    }
+
+    let participant = event.participants.find((p) => p.discordID === participantId)
+    if (participant) {
+      participant.status = status
+    } else {
+      event.participants.push({ discordID: participantId, status })
+    }
+
+    await event.save()
+    return event
+  }
 }
 
 module.exports = new EventService(Event)
