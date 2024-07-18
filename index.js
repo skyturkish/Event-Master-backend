7db5eb5bcbd0e2af828fd87dc2ac95c774f717d8
skyturkish <gokturk.acr2002@gmail.com>
2024-07-18 11:36:50 +0300

    Add health check

--- index.js
+++ index.js
@@ -13,4 +13,8 @@ app.use(bodyParser.json())
 app.use('/event', eventRouter)
 app.use('/', indexRouter)
 
+app.get('/health', (req, res) => {
+  res.send(new Date().toISOString())
+})
+
 module.exports = app
