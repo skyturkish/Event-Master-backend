9a39132a5551959f033420e82143dff1ce4eebbb
skyturkish <gokturk.acr2002@gmail.com>
2024-07-22 01:39:21 +0300

    Refactor event validation and update status handling
    - Split validateStartTime middleware into validateStartTimeForCreate and validateStartTimeForUpdate for better handling of different event creation and update scenarios.
    - Add validateStatus middleware to ensure proper status transitions for events, including checks for participant limits and status dependencies.
    - Updated relevant routes to use the new validation middlewares.

--- middleware/event-middleware.js
+++ middleware/event-middleware.js
@@ -12,16 +12,9 @@ const validateParticipantLimit = (req, res, next) => {
   next()
 }
 
-const validateStartTime = (req, res, next) => {
+const validateStartTimeForCreate = (req, res, next) => {
   const { startTime } = req.body
   const eventTime = moment(startTime)
-
-  if (req.event) {
-    const eventStartTime = moment(req.event.startTime).toISOString()
-    const formattedEventTime = eventTime.toISOString()
-
-    if (eventStartTime === formattedEventTime) return next()
-  }
 
   if (!eventTime.isValid()) return res.status(400).send({ error: 'Invalid start time format.' })
 
@@ -35,6 +28,86 @@ const validateStartTime = (req, res, next) => {
     return res.status(400).send({ error: 'You cannot create an event more than 45 days in advance.' })
 
   next()
+}
+
+const validateStartTimeForUpdate = (req, res, next) => {
+  const { startTime } = req.body
+
+  if (startTime) {
+    const eventTime = moment(startTime)
+
+    if (req.event) {
+      const eventStartTime = moment(req.event.startTime).toISOString()
+
+      const formattedEventTime = eventTime.toISOString()
+
+      if (eventStartTime === formattedEventTime) {
+        next()
+      }
+    }
+
+    if (!eventTime.isValid()) return res.status(400).send({ error: 'Invalid start time format.' })
+
+    const now = moment()
+
+    if (eventTime.isBefore(now))
+      return res.status(400).send({ error: 'You cannot interact with an event in the past.' })
+
+    const maxAdvanceTime = moment().add(45, 'days')
+
+    if (eventTime.isAfter(maxAdvanceTime))
+      return res.status(400).send({ error: 'You cannot create an event more than 45 days in advance.' })
+
+    next()
+  } else {
+    next()
+  }
+}
+
+const validateStatus = (req, res, next) => {
+  const currentParticipants = req.event.users.filter((user) => user.status === 'attending').length
+
+  const currentStatus = req.event.status
+
+  const newStatus = req.body.status
+
+  if (currentStatus == newStatus) next()
+  else {
+    if (newStatus == 'not-started')
+      return res.status(400).send({ error: 'You cannot change the status back to not-started.' })
+    else if (newStatus == 'ready-to-start') {
+      if (currentStatus == 'not-started') {
+        if (currentParticipants != req.event.participantLimit)
+          return res.status(400).send({
+            error: 'Participant limit must be equal to current number of attending users to mark as ready to start'
+          })
+      } else
+        return res.status(400).send({ error: 'Only events that have not yet started can be marked as ready-to-start.' })
+    } else if (newStatus == 'ongoing') {
+      if (currentStatus == 'not-started')
+        return res.status(400).send({ error: 'You cannot start the event that not ready to start' })
+      else if (currentStatus == 'ready-to-start') {
+        if (currentParticipants != req.event.participantLimit)
+          return res.status(400).send({
+            error: 'Participant limit must be equal to current number of attending users to mark as ready to start'
+          })
+      } else if (currentStatus == 'finished') {
+        return res.status(400).send({ error: 'You cannot start a finished event.' })
+      } else if (currentStatus == 'canceled') {
+        return res.status(400).send({ error: 'You cannot start a canceled event.' })
+      }
+    } else if (newStatus == 'finished') {
+      if (currentStatus == 'not-started' || currentStatus == 'ready-to-start')
+        return res.status(400).send({ error: 'You cannot finish an event that has not started yet.' })
+      else if (currentStatus == 'canceled')
+        return res.status(400).send({ error: 'You cannot finish a canceled event.' })
+    } else if (newStatus == 'canceled') {
+      if (currentStatus == 'ongoing') return res.status(400).send({ error: 'You cannot cancel an ongoing event.' })
+      else if (currentStatus == 'finished')
+        return res.status(400).send({ error: 'You cannot cancel a finished event.' })
+    }
+    next()
+  }
 }
 
 const validateEventExistence = async (req, res, next) => {
@@ -51,7 +124,9 @@ const asyncHandler = (fn) => (req, res, next) => Promise.resolve(fn(req, res, ne
 
 module.exports = {
   validateParticipantLimit,
-  validateStartTime,
+  validateStartTimeForCreate,
+  validateStartTimeForUpdate,
   validateEventExistence,
+  validateStatus,
   asyncHandler
 }
