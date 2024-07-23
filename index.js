2a37ea102907265ce24bc7897681993d18cdd786
skyturkish <gokturk.acr2002@gmail.com>
2024-07-23 14:00:49 +0300

    Add generic error handler middleware

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const app = express()
 
 app.use(bodyParser.json())
 
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message })
+  }
+  res.status(500).json({ error: 'Something went wrong!' })
+})
+
 app.use('/event', eventRouter)
 app.use('/', indexRouter)
 
