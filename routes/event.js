29eee122310ffca1481c85f9f39c9050d7877434
skyturkish <gokturk.acr2002@gmail.com>
2024-07-25 21:23:28 +0300

    Prevent duplicate status updates for users and add error handling

--- routes/event.js
+++ routes/event.js
@@ -51,48 +51,52 @@ router.put(
     const user = req.event.users.find((p) => p.discordID === userId)
     console.log('update user', userId, status, user)
 
-    if (req.event.status !== 'not-started' && req.event.status !== 'ready-to-start') {
-      const statusMessages = {
-        ongoing: 'This event is currently ongoing, you can no longer make a decision.',
-        finished: 'This event has already finished, you can no longer make a decision.',
-        canceled: 'This event has been canceled, you can no longer make a decision.'
+    if (user && user.status === status) {
+      res.send(req.event)
+    } else {
+      if (req.event.status !== 'not-started' && req.event.status !== 'ready-to-start') {
+        const statusMessages = {
+          ongoing: 'This event is currently ongoing, you can no longer make a decision.',
+          finished: 'This event has already finished, you can no longer make a decision.',
+          canceled: 'This event has been canceled, you can no longer make a decision.'
+        }
+
+        return res.status(400).send({ error: statusMessages[req.event.status] || 'Invalid status' })
       }
 
-      return res.status(400).send({ error: statusMessages[req.event.status] || 'Invalid status' })
+      let currentParticipants = req.event.users.filter((user) => user.status === 'attending')
+      let newStatus = status
+
+      if (
+        req.event.status === 'ready-to-start' &&
+        currentParticipants.length === req.event.participantLimit &&
+        newStatus === 'attending'
+      ) {
+        newStatus = 'waitlist'
+      }
+
+      if (user) {
+        user.status = newStatus
+      } else {
+        req.event.users.push({ discordID: userId, status: newStatus })
+      }
+
+      let event = await req.event.save()
+
+      currentParticipants = event.users.filter((user) => user.status === 'attending')
+
+      if (
+        req.event.participantLimit > currentParticipants.length &&
+        event.users.some((user) => user.status === 'waitlist')
+      ) {
+        const waitlistUser = event.users.find((user) => user.status === 'waitlist')
+        waitlistUser.status = 'attending'
+        event = await event.save()
+        // send notification to user
+      }
+
+      res.send(event)
     }
-
-    let currentParticipants = req.event.users.filter((user) => user.status === 'attending')
-    let newStatus = status
-
-    if (
-      req.event.status === 'ready-to-start' &&
-      currentParticipants.length === req.event.participantLimit &&
-      newStatus === 'attending'
-    ) {
-      newStatus = 'waitlist'
-    }
-
-    if (user) {
-      user.status = newStatus
-    } else {
-      req.event.users.push({ discordID: userId, status: newStatus })
-    }
-
-    let event = await req.event.save()
-
-    currentParticipants = event.users.filter((user) => user.status === 'attending')
-
-    if (
-      req.event.participantLimit > currentParticipants.length &&
-      event.users.some((user) => user.status === 'waitlist')
-    ) {
-      const waitlistUser = event.users.find((user) => user.status === 'waitlist')
-      waitlistUser.status = 'attending'
-      event = await event.save()
-      // send notification to user
-    }
-
-    res.send(event)
   })
 )
 
