a09253627f2a27ce185d1db31440ed16be2f6766
skyturkish <gokturk.acr2002@gmail.com>
2024-08-03 12:28:47 +0300

    Send id instead of text to discord bot

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((err, req, res, next) => {
   if (err.name === 'ValidationError') {
     return res.status(400).json({ error: err.message })
   }
-  res.status(500).json({ error: 'Something went wrong!' })
+  res.status(500).json({ error: 'somethingWentWrong' })
 })
 
 app.use('/event', eventRouter)
